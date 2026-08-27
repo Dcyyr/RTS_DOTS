bdd06265dae4ad2597f068c9ebcf9ac2cafaa099
Linus Li <l17332426256@126.com>
2026-08-27 14:21:36 +0800

    玩家可以向着鼠标位置平滑移动。

---
+++ Assets/Scripts/Mono/MouseWorldPosition.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MouseWorldPosition : MonoBehaviour
+{
+
+    public static MouseWorldPosition Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+   
+    public Vector3 GetPosition()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+        if(plane.Raycast(mouseRay,out float distance))
+        {
+            return mouseRay.GetPoint(distance);
+        }
+        else
+        {
+            return Vector3.zero; 
+        }
+    }
+}
--- Assets/Scripts/System/UnitMoverSystem.cs
+++ Assets/Scripts/System/UnitMoverSystem.cs
@@ -1,4 +1,3 @@
-using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
@@ -6,10 +5,8 @@ using Unity.Physics;
 
 partial struct UnitMoverSystem : ISystem
 {
-    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        // 将 Struct 转换为 Ref 用来读写操作
         foreach ((RefRW<LocalTransform> localTransform,
             RefRO<MoveSpeed> moveSpeed,
             RefRW<PhysicsVelocity> physicsVelocity)
@@ -17,16 +14,21 @@ partial struct UnitMoverSystem : ISystem
             RefRO<MoveSpeed>,
             RefRW<PhysicsVelocity>>())
         {
-            float3 targetPosition = localTransform.ValueRO.Position + new float3(10, 0, 0);
+            float3 targetPosition = MouseWorldPosition.Instance.GetPosition();
             float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
 
+            // 防止目标点等于当前位置时 normalize(0) 产生 NaN
+            if (math.lengthsq(moveDirection) < 0.01f)
+            {
+                physicsVelocity.ValueRW.Linear = float3.zero;
+                continue;
+            }
+
             moveDirection = math.normalize(moveDirection);
-
-            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+            float rotateSpeed = 10f;
+            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotation(moveDirection, math.up()),SystemAPI.Time.DeltaTime * rotateSpeed);
             physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.m_MoveSpeed;
-
             physicsVelocity.ValueRW.Angular = float3.zero;
-            //localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.m_MoveSpeed * SystemAPI.Time.DeltaTime;
         }
     }
 }
